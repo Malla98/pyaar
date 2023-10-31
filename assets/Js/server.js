f3e7b62629d5de38da459b2210cd412215ff2aa1
Prakash Malla <63249379+Malla98@users.noreply.github.com>
2023-10-31 11:43:11 +0000

    Migrated app into laravel

--- assets/Js/server.js
+++ assets/Js/server.js
@@ -2,5 +2,5 @@
 async function initDatabase() {
     const jsonFile = '/Js/database.json';
     const data = await fetch(jsonFile).then(response => response.json());
-    return data;
+    return ;
 }
