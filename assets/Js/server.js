f4e1aa5a286350c706efa82e373c9e430f30a92f
Prakash Malla <63249379+Malla98@users.noreply.github.com>
2023-11-03 11:53:46 +0000

    Add

--- assets/Js/server.js
+++ assets/Js/server.js
@@ -1,5 +1,28 @@
-async function loadData() {
-    const response = await fetch('assets/Js/database.json');
-    const data = await response.json();
+async function initDatabase() {
+  return await fetch('assets/Js/database.json').json();
+}
+async function iniAuth() {
+  return await fetch('assets/Js/auth.json').json();
+}
+
+function auth() {
+  var data = JSON.parse(localStorage.getItem("session"));
+  if (data !== null) {
     return data;
+  } else {
+    return null;
   }
+}
+function login(username,password){
+  var users = iniAuth();
+  var user = users.find((user) => user.username === username && user.password === password );
+  if(user){
+     localStorage.setItem("session",JSON.stringify(user));
+    return true;
+  }else{
+    return false;
+  }
+}
+function logout(){
+  localStorage.setItem("session",null);
+}
